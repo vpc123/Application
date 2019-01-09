59003a9e742e24c16d3b10340901def5c0031d49
vpc123 <212724256@qq.com>
2019-01-09 15:45:22 +0800

    普罗米修斯

---
+++ istio-image.sh
@@ -0,0 +1,44 @@
+docker pull registry.cn-hangzhou.aliyuncs.com/istio-release/citadel:1.0.0
+docker tag registry.cn-hangzhou.aliyuncs.com/istio-release/citadel:1.0.0 gcr.io/istio-release/citadel:1.0.0
+docker rmi registry.cn-hangzhou.aliyuncs.com/istio-release/citadel:1.0.0
+
+docker pull registry.cn-hangzhou.aliyuncs.com/istio-release/galley:1.0.0
+docker tag registry.cn-hangzhou.aliyuncs.com/istio-release/galley:1.0.0 gcr.io/istio-release/galley:1.0.0
+docker rmi registry.cn-hangzhou.aliyuncs.com/istio-release/galley:1.0.0
+
+docker pull registry.cn-hangzhou.aliyuncs.com/istio-release/pilot:1.0.0
+docker tag registry.cn-hangzhou.aliyuncs.com/istio-release/pilot:1.0.0 gcr.io/istio-release/pilot:1.0.0
+docker rmi registry.cn-hangzhou.aliyuncs.com/istio-release/pilot:1.0.0
+
+docker pull registry.cn-hangzhou.aliyuncs.com/istio-release/sidecar_injector:1.0.0
+docker tag registry.cn-hangzhou.aliyuncs.com/istio-release/sidecar_injector:1.0.0 gcr.io/istio-release/sidecar_injector:1.0.0
+docker rmi registry.cn-hangzhou.aliyuncs.com/istio-release/sidecar_injector:1.0.0
+
+docker pull registry.cn-hangzhou.aliyuncs.com/istio-release/proxyv2:1.0.0
+docker tag registry.cn-hangzhou.aliyuncs.com/istio-release/proxyv2:1.0.0 gcr.io/istio-release/proxyv2:1.0.0
+docker rmi registry.cn-hangzhou.aliyuncs.com/istio-release/proxyv2:1.0.0
+
+
+docker pull registry.cn-hangzhou.aliyuncs.com/istio-release/grafana:1.0.0
+docker tag registry.cn-hangzhou.aliyuncs.com/istio-release/grafana:1.0.0 gcr.io/istio-release/grafana:1.0.0
+docker rmi registry.cn-hangzhou.aliyuncs.com/istio-release/grafana:1.0.0
+
+
+
+docker pull registry.cn-hangzhou.aliyuncs.com/istio-release/mixer:1.0.0
+docker tag registry.cn-hangzhou.aliyuncs.com/istio-release/mixer:1.0.0 gcr.io/istio-release/mixer:1.0.0
+docker rmi registry.cn-hangzhou.aliyuncs.com/istio-release/mixer:1.0.0
+
+
+docker pull registry.cn-hangzhou.aliyuncs.com/istio-release/servicegraph:1.0.0
+docker tag registry.cn-hangzhou.aliyuncs.com/istio-release/servicegraph:1.0.0 gcr.io/istio-release/servicegraph:1.0.0
+docker rmi registry.cn-hangzhou.aliyuncs.com/istio-release/servicegraph:1.0.0
+
+
+docker pull registry.cn-hangzhou.aliyuncs.com/istio-release/pilot:1.0.0
+docker tag registry.cn-hangzhou.aliyuncs.com/istio-release/pilot:1.0.0 gcr.io/istio-release/pilot:1.0.0
+docker rmi registry.cn-hangzhou.aliyuncs.com/istio-release/pilot:1.0.0
+
+docker pull registry.cn-hangzhou.aliyuncs.com/istio-release/proxy_init:1.0.0
+docker tag registry.cn-hangzhou.aliyuncs.com/istio-release/proxy_init:1.0.0 gcr.io/istio-release/proxy_init:1.0.0
+docker rmi registry.cn-hangzhou.aliyuncs.com/istio-release/proxy_init:1.0.0
